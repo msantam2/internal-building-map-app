bf5a61d67cfde090a47a96c850eccfa088bcf27f
msantam2 <msantam2@gmail.com>
2017-05-06 13:22:45 -0700

    app successfully fetches JSON data from Heroku-hosted API, and create title/subtitle on landing page along with basic styles and fonts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios'; 
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { data: {} }; 
+  }
+  componentDidMount() {
+    axios.get("https://building-map-api.herokuapp.com/")
+      .then(res => {
+        console.log(res.data);
+        // this.setState({ data: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div className="app">
+        <strong><p className='app-title'>Internal Building Map App</p></strong>
+        <p className='app-subtitle'>
+          A React app that fetches data from custom Rails JSON API hosted on Heroku
         </p>
+
+        
       </div>
     );
   }
